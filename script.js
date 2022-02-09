69e4339e74480924b2cf8fb3b2ef33b566ccff8b
joaquin <jecorrea@protonmail.com>
2022-02-08 21:01:12 -0500

    full functionality for the blur added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Bring down elements
 const password = document.getElementById("password");
 const background = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  //   console.log(input);
+  //   console.log(20-length*2)
+  const blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
